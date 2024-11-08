407b235b33df8d13bf85df36607ef9ae1a91ef0a
aresCharcaC <henry.charca@tecsup.edu.pe>
2024-11-07 19:20:56 -0500

    Icono modificado estilo de Pokemon

--- app/src/main/java/com/example/lab12_googlemaps/MapScreen.kt
+++ app/src/main/java/com/example/lab12_googlemaps/MapScreen.kt
@@ -1,17 +1,27 @@
 package com.example.lab12_googlemaps
 
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Canvas
+import androidx.compose.ui.platform.LocalContext
+import androidx.core.content.ContextCompat
+import androidx.core.content.ContextCompat.getDrawable
 import com.google.android.gms.maps.model.BitmapDescriptorFactory
 import com.google.maps.android.compose.GoogleMap
 import com.google.maps.android.compose.rememberCameraPositionState
 import com.google.maps.android.compose.rememberMarkerState
 import com.google.android.gms.maps.model.LatLng
 import com.google.maps.android.compose.Marker
+import com.google.android.gms.maps.model.BitmapDescriptor
+
+
 
 
 @Composable
@@ -20,20 +30,51 @@ fun MapScreen() {
     val cameraPositionState = rememberCameraPositionState {
         position = com.google.android.gms.maps.model.CameraPosition.fromLatLngZoom(ArequipaLocation, 12f)
     }
+    val context = LocalContext.current
+
+    //Convertir el recurso drawable a BitmapDescriptor
+    val customMarker = bitmapDescriptorFromVector(
+        context = context,
+        vectorResId = R.drawable.valor // Asegúrate que este es el nombre correcto de tu imagen
+    )
+
 
 
     Box(modifier = Modifier.fillMaxSize()) {
-        // Añadir GoogleMap al layout
         GoogleMap(
             modifier = Modifier.fillMaxSize(),
             cameraPositionState = cameraPositionState
         ) {
-            // Añadir marcador en Denver, Colorado
             Marker(
                 state = rememberMarkerState(position = ArequipaLocation),
-                icon = BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_GREEN),
+                icon = customMarker,
                 title = "Arequipa, Perú"
             )
         }
+    }
+}
+
+//Función para convertir el vector/imagen a BitmapDescriptor
+fun bitmapDescriptorFromVector(context: Context, vectorResId: Int): BitmapDescriptor? {
+    return try {
+        val vectorDrawable = ContextCompat.getDrawable(context, vectorResId)
+        vectorDrawable?.let {
+            val width = it.intrinsicWidth/4
+            val height = it.intrinsicHeight/4
+
+            // Asegura que el Bitmap usa ARGB_8888 para canal alfa (transparente)
+            val bitmap = Bitmap.createBitmap(width, height, Bitmap.Config.ARGB_8888)
+            val canvas = android.graphics.Canvas(bitmap)
+
+            // Dibuja el vector en el Canvas del Bitmap
+            it.setBounds(0, 0, width, height)
+            it.draw(canvas)
+
+            // Convierte el Bitmap en BitmapDescriptor
+            BitmapDescriptorFactory.fromBitmap(bitmap)
+        }
+    } catch (e: Exception) {
+        e.printStackTrace()
+        null
     }
 }
